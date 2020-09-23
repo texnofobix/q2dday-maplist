317be9cb5fe31c29d0d904078eacc96ffb1b7095
Dustin S <texnofobix@users.noreply.github.com>
2020-09-23 07:47:15 -0400

    Create genmaplist.py

---
+++ genmaplist.py
@@ -0,0 +1,46 @@
+import os
+
+location_maps = '/home/q2user/dday/maps/' #update this with full path
+
+location_navs = '/home/q2user/dday/navigation'  #update this with full path
+
+print("""
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Dirk's Map Listing Utility</title>
+  </head>
+""")
+print("""
+<!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+""")
+print('<body>')
+
+print('<h1>Dirk\'s Map Listing Utility</h1')
+print('<ul class="list-group">')
+for file in os.listdir(location_maps):
+    #if file.endswith(".txt"):
+    basefile=(file.split('.'))[0]
+
+    if os.path.isfile(location_navs + '/' + basefile + '.nav'):
+        print('<li class="list-group-item">' + file + '</li>' )
+    else:
+        print('<li class="list-group-item">' + file +  "- nav exists </li>")
+
+
+print('</ul>')
+print('</body>')
+print('</html>')
